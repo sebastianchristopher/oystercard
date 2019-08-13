9cbcc82b7e5937ecd310768099ed5c3c8b99dec7
EloisePrasad <eloise.prasad@outlook.com>
2019-08-13 16:27:01 +0100

    day 2 on the challenge

--- oystercard/lib/oystercard.rb
+++ oystercard/lib/oystercard.rb
@@ -1,15 +1,42 @@
+MAXIMUM_BALANCE = 90
+MINIMUM_BALANCE = 1
+MINIMUM_FEE = 5
+
 class Oystercard
 
-  MAXIMUM_BALANCE = 90
-
-  attr_reader :balance
+  attr_reader :balance, :starting_station, :exit_station, :journey_history
 
   def initialize
+    # @starting_station = nil
     @balance = 0
+    @journey_history = []
+    # @in_journey = false
   end
   def top_up(num)
-    fail "#{MAXIMUM_BALANCE} is the limit" if @balance + num > MAXIMUM_BALANCE
+    fail "#{MAXIMUM_BALANCE} is the limit" if maximum?(num)
     @balance += num
+  end
+  def in_journey?
+    @starting_station.nil? ? false : true
+    # !@starting_station.nil?
+
+    # @in_journey
+  end
+  def touch_in(station)
+    fail "You need more money!" if @balance < MINIMUM_BALANCE
+    @starting_station = station
+    # @in_journey = true
+  end
+  def touch_out(station)
+    @exit_station = station
+    @journey_history.push({start: @starting_station, end: @exit_station})
+    @starting_station = nil
+    @exit_station = nil
+    deduct(MINIMUM_FEE)
+  end
+  private
+  def maximum?(num)
+    @balance + num > MAXIMUM_BALANCE
   end
   def deduct(num)
     @balance -= num
--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -1,5 +1,7 @@
 require './lib/oystercard'
 describe Oystercard do
+  let(:station) { double(:station) }
+  let(:exit_station) { double(:exit_station) }
   it 'can have a default balance of 0' do
     expect(subject.balance).to eq(0)
   end
@@ -11,15 +13,70 @@ describe Oystercard do
       expect(subject.top_up(10)).to eq(10)
     end
     it 'can raise error at limit' do
-      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      maximum_balance = MAXIMUM_BALANCE
       subject.top_up(maximum_balance)
       expect { subject.top_up(1) }.to raise_error("#{maximum_balance} is the limit")
     end
   end
   describe '#deduct' do
-    it 'can deduct balance' do
+    xit 'can deduct balance' do
       subject.top_up(20)
-      expect(subject.deduct(10)).to eq(10)
+      expect(subject.deduct(10)).to eq(15)
+    end
+  end
+  describe '#in_journey?' do
+    it 'can say it is not in journey' do
+      # expect(subject.in_journey?).to be(false)
+      expect(subject).to_not be_in_journey
+    end
+  end
+  describe '#touch_in' do
+    it 'can touch in, starting journey' do
+      subject.top_up(1)
+      subject.touch_in(station)
+      expect(subject).to be_in_journey
+    end
+    it 'can raise an error at lower limit' do
+      expect { subject.touch_in(station) }.to raise_error("You need more money!")
+    end
+    it 'can record starting station' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      expect(subject.starting_station).to eq(station)
+    end
+  end
+  describe '#touch_out' do
+    it 'can touch out, end journey' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      subject.touch_out(exit_station)
+      expect(subject).to_not be_in_journey
+    end
+    it 'erases the starting station' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      subject.touch_out(exit_station)
+      expect(subject.starting_station).to be_nil
+    end
+    it 'charges at least the minimum fee' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      # expect(subject.balance).to eq(5)
+      expect { subject.touch_out(exit_station) }.to change{ subject.balance }.by(-MINIMUM_FEE)
+    end
+    xit 'can record exit station' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      subject.touch_out(exit_station)
+      expect(subject.exit_station).to eq(exit_station)
+    end
+  end
+  describe 'it can record journeys' do
+    it 'it has a start and end journey' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      subject.touch_out(exit_station)
+      expect(subject.journey_history).to include({start: station, end: exit_station})
     end
   end
 end
